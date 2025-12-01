afa6f8da8bb04de457e83462aeed39f3285444bf
Vivi <114736085+BwunLevain@users.noreply.github.com>
2025-12-01 15:29:28 +0100

    Update Student.cs

--- data/Models/Student.cs
+++ data/Models/Student.cs
@@ -5,7 +5,7 @@ namespace SchoolClient.Models
     public class Student
     {
         [Key]
-        public int StudentId { get; set; }
+        public int StudentID { get; set; }
         [Required]
         [MaxLength(150)]
         public string StudentName { get; set; }
