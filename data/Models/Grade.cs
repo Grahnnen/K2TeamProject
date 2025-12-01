5e5c42c5df63e4114c4c27b8f1d1db5534125713
Vivi <114736085+BwunLevain@users.noreply.github.com>
2025-12-01 15:48:01 +0100

    Create Grade.cs

---
+++ data/Models/Grade.cs
@@ -0,0 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SchoolClient.Models
+{
+    public class Grade
+    {
+        [Key]
+        public int GradeID { get; set; }
+        [Required]
+        [MaxLength(150)]
+        public DateOnly GradedDate { get; set; }
+        public string GradeScale { get; set; }
+
+        //Foreign Keys
+        public int Fk_StudentID { get; set; }
+        [ForeignKey("Fk_StudentID")]
+        public Student Student { get; set; }
+
+        public int Fk_TeacherID { get; set; }
+        [ForeignKey("Fk_TeacherID")]
+        public Teacher Teacher { get; set; }
+    }
+}
