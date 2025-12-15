9b57c6eaff3a8e5d1e9ff172f4c3f753093ab2a5
Robinmarkstrom <Administratr@PC>
2025-12-15 11:57:22 +0100

    Moved structure out of main and created a seperate folder for it

---
+++ K2TeamProjectNEW/Data/AppBootstrapper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace K2TeamProjectNEW.Data
+{
+    public static class AppBootstrapper
+    {
+        public static DataService Initialize()
+        {
+            // Unicode-symboler
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+            // Ladda konfiguration från appsettings.json
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: false)
+                .Build();
+
+            var connectionString = builder.GetConnectionString("DefaultConnection");
+
+            // Skapa DatabaseFirstContext
+            var dbOptionsDatabaseFirst = new DbContextOptionsBuilder<DatabaseFirstContext>()
+                .UseSqlServer(connectionString)
+                .Options;
+            var databaseFirst = new DatabaseFirstContext(dbOptionsDatabaseFirst);
+
+            // Skapa CodeFirstContext
+            var dbOptionsCodeFirst = new DbContextOptionsBuilder<CodeFirstContext>()
+                .UseSqlServer(connectionString)
+                .Options;
+            var codeFirst = new CodeFirstContext(dbOptionsCodeFirst);
+
+            // Skapa och returnera DataService
+            return new DataService(codeFirst, databaseFirst);
+        }
+    }
+}
